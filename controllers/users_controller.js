e20b924645bcbe73cf711a28db702fd2a1ecd9d8
Bhargav-SH <cherukupallibhargav@gmail.com>
2023-06-19 01:30:18 +0530

    added ejs

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile = function(req,res)
+{
+    res.end('<h1>User Profile</h1>');
+}
+//now this controller is ready to be accesed by a router
+//now that route need to be accesed by this browser
+//action returns data
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ const homecontroller = require('../controllers/home_controller')
 console.log(('router loaded'));
 
 router.get('/', homecontroller.home);
+router.use('/users', require('./users'));
+
+//for any further routes acces from here
+//router.use('/routerName' , require('./routerfile) );
 
 module.exports = router; //app.get app.post  handled by this module
