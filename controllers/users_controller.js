934a0eb9c03280ff53633cd2759b11aecc9e00a0
Bhargav-SH <cherukupallibhargav@gmail.com>
2023-07-08 03:16:54 +0530

    addded models a

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,151 @@
+const User = require('../models/user')
+
 module.exports.profile = function(req,res)
 {
     // res.end('<h1>User Profile</h1>');
-    return res.render('user_profile', {
-        title: "User Profile"
-    });
+    // return res.render('user_profile', {
+    //     title: "User Profile"
+    // });
+
+    if(req.cookies.user_id)
+    {
+            User.findById(req.cookies.user_id)
+            .then((user)=>{
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                });
+                    
+                })
+                .catch((err)=>{
+                    console.log('error in creating user');
+                    return;
+                    })
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 //now this controller is ready to be accesed by a router
 //now that route need to be accesed by this browser
 //action returns data
+
+
+//render the signup page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+
+
+}
+
+//render the signin page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
+
+//get the sigup data
+
+module.exports.create = function(req,res){
+//checking 
+    if(req.body.password !=req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email:req.body.email})
+    .then((user)=>{
+        if(!user){
+            User.create(req.body)
+        .then((user)=>{
+        return res.redirect('/users/sign-in');
+            
+        })
+        .catch((err)=>{
+            console.log('error in creating user');
+            return;
+            })
+        }else
+            return res.redirect('back');
+        
+    })
+    .catch((err)=>{
+        console.log('error in finding user');
+    })
+}
+    
+
+    // User.find({email:req.body.email})
+    // .then(data=> {
+    //     console.log(data)
+    // })
+    // .catch(error=>{
+    //     console.log(error)
+    // });
+
+
+
+
+
+
+//sign in and create the session for the user
+module.exports.createSession = function(req,res){
+    //TODO LATER
+    //steps to authenticate
+    //find the user
+    // User.findOne({email: req.body.email})
+    // //handling error with promise
+    // .then((user)=>{
+    //     //handle is user found
+    //     if(user){
+    //         //handle password which dont match
+    //         if(user.password!=req.body.password){
+    //             console.log('password mismatch');
+    //             return res.redirect('back');
+    //         }
+    //           //handle session creation
+    //           res.cookie('user_id', user_id);
+    //           return res.redirect('/users/profile');
+
+    //     }
+    //      //handle user not found
+    //     else{
+    //     console.log('no user found');
+    //     return res.redirect('back');
+    //     }
+    // })
+    // .catch((err)=>{
+    //     console.log('error in finding user');
+    //     return res.redirect('back');
+    // })
+
+
+    // User.findOne({email:req.body.email})
+    // //handling error with promise
+    // .then((user)=>{
+    //     //handling if user is found
+    //       if(user){
+    //         //handling password check after finding user
+    //          if(user.password!=req.body.password){
+    //             console.log('password mismatch');
+    //             return res.redirect('back');
+    //          }
+    //          //creating session after finding email and password
+    //          res.cookie('user_id',user.id);
+    //          return res.redirect('/users/profile');
+    //       }
+    //       //if user itself is not found
+    //       else{
+    //         console.log('no user found');
+    //         return res.redirect('back');
+    //       }
+    // })
+    // //if error occurs
+    // .catch((err)=>{
+    //     console.log('error in finding user');
+    //     return res.redirect('back');
+    // })
+
+
+}
